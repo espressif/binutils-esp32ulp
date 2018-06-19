30e18aabde86d9789b4a6d812e6bd937237c422b
Dmitry <Dmitry@espressif.com>
2018-06-19 11:58:07 +0300

    Auto tests included into CI. Tests have to mark changed instructions.

---
+++ gas/testsuite/gas/esp32ulp/check_all.sh
@@ -0,0 +1,9 @@
+./gas/as-new -al ./gas/testsuite/gas/esp32ulp/i2c_wr.s > ./gas/testsuite/gas/esp32ulp/compare/i2c_wr.lst
+./gas/as-new -al ./gas/testsuite/gas/esp32ulp/esp32ulp_all.s > ./gas/testsuite/gas/esp32ulp/compare/esp32ulp_all.lst
+./gas/as-new -al ./gas/testsuite/gas/esp32ulp/esp32ulp_globals.s > ./gas/testsuite/gas/esp32ulp/compare/esp32ulp_globals.lst
+./gas/as-new -al ./gas/testsuite/gas/esp32ulp/esp32ulp_ranges.s > ./gas/testsuite/gas/esp32ulp/compare/esp32ulp_ranges.lst
+./gas/as-new -al ./gas/testsuite/gas/esp32ulp/test_and.s > ./gas/testsuite/gas/esp32ulp/compare/test_and.lst
+./gas/as-new -al ./gas/testsuite/gas/esp32ulp/test_jumpr.s > ./gas/testsuite/gas/esp32ulp/compare/test_jumpr.lst
+./gas/as-new -al ./gas/testsuite/gas/esp32ulp/test_reg.s > ./gas/testsuite/gas/esp32ulp/compare/test_reg.lst
+./gas/as-new -al ./gas/testsuite/gas/esp32ulp/test1.s > ./gas/testsuite/gas/esp32ulp/compare/test1.lst
+./gas/as-new -al ./gas/testsuite/gas/esp32ulp/test_macro.s > ./gas/testsuite/gas/esp32ulp/compare/test_macro.lst
