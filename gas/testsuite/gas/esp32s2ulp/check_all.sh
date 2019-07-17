e5588abeb4d475afd8cd34a64c47ee152cc59795
Anton Maklakov <anton@espressif.com>
2019-07-17 13:40:35 +0700

    ci: Fixup archive naming, generalize CI script, upd macos image

--- gas/testsuite/gas/esp32s2ulp/check_all.sh
+++ gas/testsuite/gas/esp32s2ulp/check_all.sh
@@ -1,14 +1,29 @@
-./gas/as-new -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_st.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_st.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_regrw.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_regrw.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_i2crw.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_i2crw.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_wait.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_wait.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_adc.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_adc.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_alu.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_alu.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_jump.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_jump.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_wakehalt.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_wakehalt.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_tsens.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_tsens.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_sleep.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_sleep.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_ld.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_ld.lst
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+AS="./gas/as-new"
+
+if [ -n "${1:-}" ]; then
+	PREFIX="$1"
+	AS="${PREFIX}as"
+fi
+
+echo "Testing $AS ..."
+
+${AS} -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_st.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_st.lst
+${AS} -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_regrw.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_regrw.lst
+${AS} -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_i2crw.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_i2crw.lst
+${AS} -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_wait.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_wait.lst
+${AS} -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_adc.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_adc.lst
+${AS} -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_alu.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_alu.lst
+${AS} -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_jump.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_jump.lst
+${AS} -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_wakehalt.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_wakehalt.lst
+${AS} -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_tsens.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_tsens.lst
+${AS} -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_sleep.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_sleep.lst
+${AS} -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_ld.s > ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_ld.lst
 # This part used to check gas and linker together
 #./gas/as-new -al ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_st.s -o ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_st.elf
 #./ld/ld-new.exe -o ./gas/testsuite/gas/esp32s2ulp/compare/esp32s2ulp_st.bin -A elf32-esp32s2ulp ./gas/testsuite/gas/esp32s2ulp/esp32s2ulp_st.elf
--- gas/testsuite/gas/esp32ulp/check_all.sh
+++ gas/testsuite/gas/esp32ulp/check_all.sh
@@ -1,9 +1,24 @@
-./gas/as-new -al ./gas/testsuite/gas/esp32ulp/i2c_wr.s > ./gas/testsuite/gas/esp32ulp/compare/i2c_wr.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32ulp/esp32ulp_all.s > ./gas/testsuite/gas/esp32ulp/compare/esp32ulp_all.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32ulp/esp32ulp_globals.s > ./gas/testsuite/gas/esp32ulp/compare/esp32ulp_globals.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32ulp/esp32ulp_ranges.s > ./gas/testsuite/gas/esp32ulp/compare/esp32ulp_ranges.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32ulp/test_and.s > ./gas/testsuite/gas/esp32ulp/compare/test_and.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32ulp/test_jumpr.s > ./gas/testsuite/gas/esp32ulp/compare/test_jumpr.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32ulp/test_reg.s > ./gas/testsuite/gas/esp32ulp/compare/test_reg.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32ulp/test1.s > ./gas/testsuite/gas/esp32ulp/compare/test1.lst
-./gas/as-new -al ./gas/testsuite/gas/esp32ulp/test_macro.s > ./gas/testsuite/gas/esp32ulp/compare/test_macro.lst
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+AS="./gas/as-new"
+
+if [ -n "${1:-}" ]; then
+	PREFIX="$1"
+	AS="${PREFIX}as"
+fi
+
+echo "Testing $AS ..."
+
+${AS} -al ./gas/testsuite/gas/esp32ulp/i2c_wr.s > ./gas/testsuite/gas/esp32ulp/compare/i2c_wr.lst
+${AS} -al ./gas/testsuite/gas/esp32ulp/esp32ulp_all.s > ./gas/testsuite/gas/esp32ulp/compare/esp32ulp_all.lst
+${AS} -al ./gas/testsuite/gas/esp32ulp/esp32ulp_globals.s > ./gas/testsuite/gas/esp32ulp/compare/esp32ulp_globals.lst
+${AS} -al ./gas/testsuite/gas/esp32ulp/esp32ulp_ranges.s > ./gas/testsuite/gas/esp32ulp/compare/esp32ulp_ranges.lst
+${AS} -al ./gas/testsuite/gas/esp32ulp/test_and.s > ./gas/testsuite/gas/esp32ulp/compare/test_and.lst
+${AS} -al ./gas/testsuite/gas/esp32ulp/test_jumpr.s > ./gas/testsuite/gas/esp32ulp/compare/test_jumpr.lst
+${AS} -al ./gas/testsuite/gas/esp32ulp/test_reg.s > ./gas/testsuite/gas/esp32ulp/compare/test_reg.lst
+${AS} -al ./gas/testsuite/gas/esp32ulp/test1.s > ./gas/testsuite/gas/esp32ulp/compare/test1.lst
+${AS} -al ./gas/testsuite/gas/esp32ulp/test_macro.s > ./gas/testsuite/gas/esp32ulp/compare/test_macro.lst
