eb588ed15e8072c439ff452598d25b602d0cd20b
Blake Felt <blake.w.felt@gmail.com>
2022-09-06 16:33:08 -0600

    Added BFD_RELOC_32 for esp32s2ulp.

--- bfd/elf32-esp32s2ulp.c
+++ bfd/elf32-esp32s2ulp.c
@@ -640,6 +640,20 @@ static reloc_howto_type esp32s2ulp_howto_table[] =
 	(0x3 << 4),	                    /* dst_mask.  */
 	FALSE),			                /* pcrel_offset.  */
 
+	HOWTO(R_BFD_RELOC_32,           /* type.  */
+	0,			                    /* rightshift.  */
+	2,			                    /* size (0 = byte, 1 = short, 2 = long).  */
+	32,			                    /* bitsize.  */
+	FALSE,			                /* pc_relative.  */
+	0,			                    /* bitpos.  */
+	complain_overflow_bitfield,     /* complain_on_overflow.  */
+	NULL,		                    /* special_function.  */
+	"32",	                        /* name.  */
+	FALSE,			                /* partial_inplace.  */
+	0xffffffff,                     /* src_mask.  */ // dya-pass - 0
+	0xffffffff,	                    /* dst_mask.  */
+	FALSE),			                /* pcrel_offset.  */
+
 
 	HOWTO(R_ESP32S2ULP_RESERVE,	/* type.  */
 	0,			/* rightshift.  */
@@ -759,6 +773,8 @@ static const struct esp32s2ulp_reloc_map esp32s2ulp_reloc_map[] =
 	{ BFD_RELOC_ESP32S2ULP_I2C_RW_SEL, R_ESP32S2ULP_I2C_RW_SEL },
 
 	{ BFD_RELOC_ESP32S2ULP_WR_MEM_LABEL, R_ESP32S2ULP_WR_MEM_LABEL },
+
+	{ BFD_RELOC_32, R_BFD_RELOC_32 },
 
 	{ BFD_RELOC_ESP32S2ULP_RESERVE, R_ESP32S2ULP_RESERVE },
 
